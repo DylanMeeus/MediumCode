9856cd37582d87bf996ac5814ec158798628c142
dylan <meeusdylan@protonmail.com>
2019-12-30 23:38:17 +0100

    Add classic count distinct

--- hyperloglog/main.go
+++ hyperloglog/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"encoding/binary"
 	"hash/fnv"
 	"math/bits"
+	"fmt"
+	"math/rand"
 )
 
 func main() {
-	fmt.Printf("%v\n", leftmostActiveBit(uint32(16)))
+	bs,is:= getRandomData()
+	dt := classicCountDistinct(is)
+	h := NewHyperLogLog(64)
+	for _,b := range bs {
+		h.Add(b)
+	}
+	fmt.Printf("%v\n", dt)
+}
+
+// get random uint32s as a [][]byte slice
+func getRandomData() (out [][]byte, intout []uint32) {
+	for i := 0; i < 10;i++ {
+		i := rand.Uint32()
+		b := make([]byte, 4)
+		binary.LittleEndian.PutUint32(b, i)
+		out = append(out, b)
+		intout = append(intout, i)
+	}
+	return
+}
+
+func classicCountDistinct(input []uint32) int {
+	m := map[uint32]struct{}{}
+	for _,i := range input {
+		if _, ok := m[i]; !ok {
+			m[i] = struct{}{}
+		}
+	}
+	return len(m)
 }
 
 type hyperLogLog struct {
-	registers []uint
+	registers []int
 	m         uint // number of registers
 	b         uint // bits to calculate [4..16]
 }
@@ -19,17 +49,21 @@ type hyperLogLog struct {
 // Initialize hyperLogLog with m registers
 func NewHyperLogLog(m uint) hyperLogLog {
 	return hyperLogLog{
-		registers: make([]uint, m),
+		registers: make([]int, m),
 		m:         m,
 	}
 }
 
 
 func (h hyperLogLog) Add(data []byte) hyperLogLog {
-	hash := createHash(data)
-	k := 32 - h.b
-	_ = k
-	_ = hash
+	x := createHash(data)
+	k := 32 - h.b // first b bits
+	r := leftmostActiveBit(x << h.b)
+	j := x >> uint(k)
+
+	if r > h.registers[j] {
+		h.registers[j] = r
+	}
 	return h
 }
 

5d9fa8e1b0dc4a4113f7f753731ee3d695be09ad
Dylan Meeus <meeusdylan@protonmail.com>
2019-12-30 21:59:43 +0100

    Add power2 function

--- hyperloglog/main.go
+++ hyperloglog/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println("starting up")
+	fmt.Printf("%v\n", leftBitPosition(uint32(10)))
 }
 
 type hyperLogLog struct {
@@ -23,6 +23,27 @@ func NewHyperLogLog(m uint) hyperLogLog {
 	}
 }
 
+
+func (h hyperLogLog) Add(data []byte) hyperLogLog {
+	hash := createHash(data)
+	k := 32 - h.b
+	_ = k
+	_ = hash
+	return h
+}
+
+
+func leftBitPosition(x uint32) uint32 {
+	// get the previous power of 2
+	x = x | (x >> 1)
+	x = x | (x >> 2)
+	x = x | (x >> 4)
+	x = x | (x >> 8)
+	x = x | (x >> 16)
+	return x - (x >> 1)
+}
+
+
 // create a 32-bit hash
 func createHash(stream []byte) uint32 {
 	h := fnv.New32()
@@ -31,3 +52,4 @@ func createHash(stream []byte) uint32 {
 	h.Reset()
 	return sum
 }
+

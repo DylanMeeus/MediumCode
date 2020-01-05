e6aa1d5d8027b78ad4f5e33c2abc8823911f8d25
dylan <meeusdylan@protonmail.com>
2020-01-05 20:38:37 +0100

    Add alpha correction to count algorithm

--- hyperloglog/main.go
+++ hyperloglog/main.go
@@ -4,23 +4,26 @@ import (
 	"encoding/binary"
 	"hash/fnv"
 	"math/bits"
+	"math"
 	"fmt"
 	"math/rand"
 )
 
 func main() {
-	bs,is:= getRandomData()
+	bs, is := getRandomData()
 	dt := classicCountDistinct(is)
-	h := NewHyperLogLog(64)
-	for _,b := range bs {
+	h := NewHyperLogLog(32)
+	for _, b := range bs {
 		h.Add(b)
 	}
-	fmt.Printf("%v\n", dt)
+	hd := h.Count()
+	fmt.Printf("classic estimate: %v\n", dt)
+	fmt.Printf("hyperloglog estimate: %v\n", hd)
 }
 
 // get random uint32s as a [][]byte slice
 func getRandomData() (out [][]byte, intout []uint32) {
-	for i := 0; i < 10;i++ {
+	for i := 0; i < 100000; i++ {
 		i := rand.Uint32()
 		b := make([]byte, 4)
 		binary.LittleEndian.PutUint32(b, i)
@@ -32,7 +35,7 @@ func getRandomData() (out [][]byte, intout []uint32) {
 
 func classicCountDistinct(input []uint32) int {
 	m := map[uint32]struct{}{}
-	for _,i := range input {
+	for _, i := range input {
 		if _, ok := m[i]; !ok {
 			m[i] = struct{}{}
 		}
@@ -51,9 +54,9 @@ func NewHyperLogLog(m uint) hyperLogLog {
 	return hyperLogLog{
 		registers: make([]int, m),
 		m:         m,
+		b:         uint(math.Ceil(math.Log2(float64(m)))),
 	}
 }
-
 
 func (h hyperLogLog) Add(data []byte) hyperLogLog {
 	x := createHash(data)
@@ -67,11 +70,20 @@ func (h hyperLogLog) Add(data []byte) hyperLogLog {
 	return h
 }
 
+func (h *hyperLogLog) Count() uint64 {
+	sum := 0.
+	m := float64(h.m)
+	for _, v := range h.registers {
+		sum += 1. / math.Pow(2, float64(v))
+	}
+	estimate := 0.697 * m * m / sum
+	fmt.Printf("estimate: %v\n", estimate)
+	return uint64(estimate)
+}
 
 func leftmostActiveBit(x uint32) int {
 	return 1 + bits.LeadingZeros32(x)
 }
-
 
 // create a 32-bit hash
 func createHash(stream []byte) uint32 {
@@ -81,4 +93,3 @@ func createHash(stream []byte) uint32 {
 	h.Reset()
 	return sum
 }
-

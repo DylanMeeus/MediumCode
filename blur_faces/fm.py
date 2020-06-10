d3e101a783539d94ee29a65670b530e9cc5dfc04
Dylan Meeus <meeusdylan@protonmail.com>
2020-06-10 19:24:19 +0200

    Clean up code

--- blur_faces/fm.py
+++ blur_faces/fm.py
@@ -10,27 +10,17 @@ import requests
 
 dploy_url = "https://3345423f-8bbf-4215-8004-215a3da67b60.users.dploy.ai/"
 
-def resize_img(img, BASEWIDTH):
-    wpercent = (BASEWIDTH / float(img.shape[1]))
-    print(wpercent)
+def resize_img(img, new_width):
+    wpercent = (new_width / float(img.shape[1]))
     wsize = int(img.shape[1] * wpercent)
     hsize = int(img.shape[0] * wpercent)
     dim = (wsize, hsize)
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
     return img
 
-
-if __name__ == '__main__':
-    # set up our tokens
-    # api_key = os.environ['apikey']
-    # user_id = os.environ['userid']
-    api_key = "L4XSWSKVMFBTBFSLMTWBZ7I6LQ"
-    user_id = "auth0|5ecb72629c5cbe0c1ac5c11e"
-
-    # parse our command-line argument
-    file_name = (sys.argv[-1])
-    extension = file_name.split(".")[1]
-
+def detect_unmasked(file_name):
+    api_key = os.environ['apikey']
+    user_id = os.environ['userid']
     with open(file_name, "rb") as img:
         encoded_image = base64.b64encode(img.read())
 
@@ -41,13 +31,22 @@ if __name__ == '__main__':
             'x-api-user': user_id,
             }
     r = requests.post(url = dploy_url, json={"image": encoded_image, "type": extension}, headers = headers)
+    print(r.text)
     data = json.loads(r.text)
 
 
     # get bounding boxes and results
     bbox, result, annotated_img = data['detected_face_coordinates'], data['detected_face_labels'], data['annotated_image']
-    not_masked = list(map(lambda k: k[0], filter(lambda x: x[1] == 'not masked', (zip(bbox, result)))))
-    print(not_masked)
+    not_masked_bbox = list(map(lambda k: k[0], filter(lambda x: x[1] == 'not masked', (zip(bbox, result)))))
+    return (not_masked_bbox, annotated_img)
+
+
+if __name__ == '__main__':
+    # parse our command-line argument
+    file_name = (sys.argv[-1])
+    extension = file_name.split(".")[1]
+
+    not_masked, annotated_img = detect_unmasked(file_name)
 
     imdata = base64.b64decode(str(annotated_img))
     returned_img = Image.open(io.BytesIO(imdata))
@@ -56,9 +55,6 @@ if __name__ == '__main__':
     if open_cv_img.shape[0] != returned_img.width:
         open_cv_img = resize_img(open_cv_img, returned_img.width)
 
-    print(open_cv_img.shape)
-        
-
 
     kernel = (41,41) # how blurry it will be
     for face in not_masked: 
@@ -66,7 +62,7 @@ if __name__ == '__main__':
         # rescale it 
         open_cv_img[y1:y2, x1:x2] = cv2.blur(open_cv_img[y1:y2, x1:x2], kernel)
 
-    cv2.imwrite("./blur.jpeg", open_cv_img)
+    cv2.imwrite(file_name + "_blurred" + "." + extension, open_cv_img)
     cv2.imshow("resized", open_cv_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()

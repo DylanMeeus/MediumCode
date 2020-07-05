8e01cbd7c8a67d50401ed85cc148308df95bef38
Dylan <meeusdylan@protonmail.com>
2020-07-05 14:28:07 +0200

    Add sine code

---
+++ Audio/Sine/main.go
@@ -0,0 +1,31 @@
+package main
+
+/* program to create a pitch perfect (440Hz) sound */
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+const (
+	nsamps = 50
+)
+
+var (
+	tau = math.Pi * 2
+)
+
+func main() {
+	fmt.Fprintf(os.Stderr, "generating sine wave..\n")
+	generate()
+	fmt.Fprintf(os.Stderr, "done")
+}
+
+func generate() {
+	var angle float64 = tau / nsamps
+	for i := 0; i < nsamps; i++ {
+		samp := math.Sin(angle * float64(i))
+		fmt.Printf("%.81f\t%81.f\n", samp, samp)
+	}
+}
